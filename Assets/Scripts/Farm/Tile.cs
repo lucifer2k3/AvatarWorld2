d6c2b395f7c218aa231b4527e9effe2a811da6f2
lucifer2k3 <113585945+lucifer2k3@users.noreply.github.com>
2024-12-24 16:11:25 +0700

    24/12

--- Assets/Scripts/Farm/Tile.cs
+++ Assets/Scripts/Farm/Tile.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.U2D.Animation;
 
 public class Tile : MonoBehaviour
 {
+    private SpriteLibrary spriteLibrary;
+    private SpriteRenderer renderer;
+    [SerializeField]private SpriteRenderer interaction;
 
-    // Update is called once per frame
+
+    public bool isWatered = false;
+    public bool canPlant = false;
+    public int state = 0;
+
+    private void Awake()
+    {
+        this.spriteLibrary = GetComponent<SpriteLibrary>();
+        this.renderer = GetComponent<SpriteRenderer>();
+    }
+    private void FixedUpdate()
+    {
+        if (state == 0)
+        {
+
+        }
+    }
     void OnMouseEnter(){
-
+        interaction.sprite = spriteLibrary.GetSprite("Dirt", "r1"); 
     }
     void OnMouseExit(){
-
+        interaction.sprite = null;
     }
 }
