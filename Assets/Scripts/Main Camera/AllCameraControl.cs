76a108f0c9897b281fc678f8e135806f48e73f6d
lucifer2k3 <113585945+lucifer2k3@users.noreply.github.com>
2025-02-13 17:01:26 +0700

    13/02 p1

--- Assets/Scripts/Main Camera/AllCameraControl.cs
+++ Assets/Scripts/Main Camera/AllCameraControl.cs
@@ -10,30 +10,44 @@ public class AllCameraControl : MonoBehaviour
     public CinemachineVirtualCamera farm_Camera;
     public CinemachineVirtualCamera hillyTown_Camera;
     public CinemachineVirtualCamera robertStore_Camera;
-    public static int playerpos = 0;
+    public CinemachineVirtualCamera farm_house;
+   
+    public static int playerpos = 0;//(farm house = 3; robert store =2 hilly town =1; farm =0)
+
 
     private void Start()
     {    }
     private void Update()
     {
+        // priority = 1 -> do uu tien camera cua map day = 1
         switch (playerpos)
         {
             case 0:
+                farm_house.Priority = 0;
                 hillyTown_Camera.Priority = 0;
                 robertStore_Camera.Priority = 0;
                 farm_Camera.Priority = 1;
             break;
             case 1:
+                farm_house.Priority = 0;
                 farm_Camera.Priority = 0;
                 robertStore_Camera.Priority = 0;
                 hillyTown_Camera.Priority = 1;
             break;
             case 2:
+                farm_house.Priority = 0;
                 farm_Camera.Priority = 0;
                 hillyTown_Camera.Priority = 0;
                 robertStore_Camera.Priority = 1;
 
+            break;
+            case 3:
+                farm_Camera.Priority = 0;
+                hillyTown_Camera.Priority = 0;
+                robertStore_Camera.Priority = 0;
+                farm_house.Priority = 1;
                 break;
+
         }
     }
 }
